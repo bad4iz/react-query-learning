85528013f337f347bc96bdd64a703bdafac230e4
bad4iz <developer@bad4iz.ru>
2021-08-15 18:56:27 +0400

    реализуем подобие пагинации

--- src/Films.js
+++ src/Films.js
@@ -1,9 +1,41 @@
 import { useState } from 'react';
+import FilmPage from './FilmPage';
+import { useQuery } from 'react-query';
+
+const useGetFilms = () =>
+  useQuery(['films'], () =>
+    fetch('http://swapi.dev/api/films').then(
+      (res) => res.json(),
+    ),
+  );
 
 const Films = ({ queryKey }) => {
-  const [film, setFilm] = useState('');
+  const [filmUrl, setFilmUrl] = useState('');
 
-  return <div>film</div>;
+  const { data } = useGetFilms();
+
+  return filmUrl ? (
+    <>
+      <button onClick={() => setFilmUrl('')}>
+        back
+      </button>
+      <FilmPage url={filmUrl} />
+    </>
+  ) : (
+    <ul>
+      {data?.results?.map((film) => (
+        <li key={film.url}>
+          <b>Film:</b>
+          <a
+            href="#"
+            onClick={() => setFilmUrl(film.url)}
+          >
+            {film.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Films;
