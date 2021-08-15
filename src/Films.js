e66ef69b36f7c76acf243b553827ff190bea0efb
bad4iz <developer@bad4iz.ru>
2021-08-15 21:52:21 +0400

    инициализируем наши данные из кэша #2

--- src/Films.js
+++ src/Films.js
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import FilmPage from './FilmPage';
 import { useQuery } from 'react-query';
+import { queryClient } from './App';
 
 const useGetFilms = () =>
   useQuery(['films'], () =>
-    fetch('http://swapi.dev/api/films').then(
-      (res) => res.json(),
-    ),
+    fetch('http://swapi.dev/api/films')
+      .then((res) => res.json())
+      .then(({ results }) => {
+        results.forEach((film) =>
+          queryClient.setQueryData(
+            ['film', film.url],
+            film,
+          ),
+        );
+        return results;
+      }),
   );
 
 const Films = ({ queryKey }) => {
@@ -23,7 +32,7 @@ const Films = ({ queryKey }) => {
     </>
   ) : (
     <ul>
-      {data?.results?.map((film) => (
+      {data?.map((film) => (
         <li key={film.url}>
           <b>Film:</b>
           <a
