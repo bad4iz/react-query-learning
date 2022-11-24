b4218730049e9b7835a692308370736fbeaae047
bad4iz <developer@bad4iz.ru>
2022-11-24 07:48:51 +0400

    39. Бесконечный запрос при скроле

--- src/Todos.js
+++ src/Todos.js
@@ -1,15 +1,33 @@
 import {
-  useInfiniteQuery,
-  useQueryClient,
-} from 'react-query';
-import { Link } from 'react-router-dom';
-import React from 'react';
+  useInfiniteQuery
+} from "react-query";
+import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
 
 const fetchTodos = async ({ pageParam }) => {
   return fetch(
-    `api/todos?page=${pageParam}`,
+    `api/todos?page=${pageParam}`
   ).then((res) => res.json());
 };
+
+const useInfiniteQueryScrolling = async (container, callback, offset = 0) => {
+  const callbackRef = useRef(() => callback);
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+  useEffect(() => {
+    const onScroll = () => {
+      const scrollContainer = container === document ? document.scrollingElement : container;
+      if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.clientHeight - offset) {
+        callbackRef.current();
+      }
+    };
+
+    container.addEventListener("scroll", onScroll, { passive: true });
+    return () => container.removeEventListener("scroll", onScroll);
+  }, [container, offset]);
+};
+
 export const Todos = () => {
   const {
     data: todos = [],
@@ -17,47 +35,47 @@ export const Todos = () => {
     isFetching,
     fetchNextPage,
     hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteQuery(['todos'], fetchTodos, {
+    isFetchingNextPage
+  } = useInfiniteQuery(["todos"], fetchTodos, {
     getNextPageParam: (lastPage, pages) => {
       if (lastPage.length === 0) {
         return undefined;
       }
       return pages.length + 1;
-    },
+    }
   });
-  const queryClient = useQueryClient();
+
+  useInfiniteQueryScrolling(document, () => {
+    if (!isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, 500);
 
   return isLoading ? (
-    'загрузка...'
+    "загрузка..."
   ) : (
     <div>
       <h3>
-        Мой список дел{isFetching ? '...' : null}
+        Мой список дел{isFetching ? "..." : null}
       </h3>
       <ul>
         {todos?.pages.map((page, index) => (
           <React.Fragment key={index}>
             {page.map((todo) => (
-              <ul key={todo.id}>
+              <li key={todo.id}>
                 <Link to={`/todo/${todo.id}`}>
                   {todo.name}
                 </Link>
-              </ul>
+              </li>
             ))}
           </React.Fragment>
         ))}
+        <li key={"load"}>
+          {isFetchingNextPage ? " Загрузка...." : ""}
+        </li>
       </ul>
 
-      <button
-        onClick={fetchNextPage}
-        disabled={
-          !hasNextPage || isFetchingNextPage
-        }
-      >
-        следующая
-      </button>
-      {isFetchingNextPage ? 'Загрузка' : '' + ''}
+
     </div>
   );
 };
